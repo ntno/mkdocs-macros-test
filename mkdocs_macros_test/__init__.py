b31c384d51f77426cb45dc45b5f76693436d0c1c
ntno <ntno@users.noreply.github.com>
2023-01-16 20:23:31 -0500

    flake fixes

--- mkdocs_macros_test/__init__.py
+++ mkdocs_macros_test/__init__.py
@@ -1,14 +1,14 @@
-"""
-Code
-"""
 
-def test_fn(x:float):
+
+def test_fn(x: float):
     "Test function"
     return x * 4 / 3
 
-def say_hello(s:str):
+
+def say_hello(s: str):
     "Test procedure"
     return "<i>Hello %s</i>" % s
+
 
 def define_env(env):
     "Declare environment for jinja2 templates for markdown"
@@ -16,8 +16,7 @@ def define_env(env):
     for fn in [test_fn, say_hello]:
         env.macro(fn)
 
-
     # you could, of course, also define a macro here:
     @env.macro
-    def test_fn2(s:str):
+    def test_fn2(s: str):
         return "I am displaying this: %s" % s
